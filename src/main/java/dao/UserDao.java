88be03c5f76e579c794ef656d8befd1bd2b8bea8
Louiskoyio <koyiolouis@gmail.com>
2019-09-23 14:48:20 +0300

    Added DAO files

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,12 @@
 package dao;
+import models.User;
+
+import java.util.List;
 
 public interface UserDao {
+    List<User> getAll();
+    void addUser(User user);
+    void editUser(int user_id);
+    void deleteUser(int user_id);
+
 }
