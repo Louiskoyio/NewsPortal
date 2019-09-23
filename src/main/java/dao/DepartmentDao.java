9d742b1ec1d3ba04dcad8114033fd09becd243ba
Louiskoyio <koyiolouis@gmail.com>
2019-09-23 14:44:51 +0300

    Added DAO files

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,12 @@
 package dao;
 
+import models.Department;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    List<Department> getAll();
+    void addDepartment(Department department);
+    List departmentNews(int department_id);
+    void deleteDepartment(int department_id);
 }
--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,13 @@
 package dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+
+    List<News> getAll();
+    void addNews(News news);
+    void editNews(int news_id);
+    void deleteNews(int news_id);
 }
