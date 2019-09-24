f122836a767dc02a7e7cbf8e71654fdd1729b75f
Louiskoyio <koyiolouis@gmail.com>
2019-09-24 11:06:30 +0300

    Set Up API initial structure

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,15 @@ import dao.Sql2oDepartmentDao;
 import dao.Sql2oNewsDao;
 import dao.Sql2oUserDao;
 import models.Department;
+import models.News;
+import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -15,7 +22,7 @@ public class App {
         Sql2oNewsDao newsDao;
         Connection conn;
         Gson gson = new Gson();
-        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        String connectionString = "jdbc:h2:~/news_portal.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
 
         departmentDao = new Sql2oDepartmentDao(sql2o);
@@ -23,13 +30,41 @@ public class App {
         newsDao = new Sql2oNewsDao(sql2o);
         conn = sql2o.open();
 
+        post("/users/new", "application/json", (req, res) -> {
+            User user = gson.fromJson(req.body(), User.class);
+            userDao.add(user);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(user);
+        });
 
-        post("/restaurants/new", "application/json", (req, res) -> {
+        post("/news/new", "application/json", (req, res) -> {
+            News news = gson.fromJson(req.body(), News.class);
+            newsDao.add(news);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(news);
+        });
+
+        post("/departments/new", "application/json", (req, res) -> {
             Department department = gson.fromJson(req.body(), Department.class);
             departmentDao.add(department);
             res.status(201);
             res.type("application/json");
             return gson.toJson(department);
         });
+
+        get("/news", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            return gson.toJson(newsDao.getAll());//send it back to be displayed
+        });
+
+        get("/users/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            int employee_id = Integer.parseInt(req.params("employee_id"));
+            res.type("application/json");
+            return gson.toJson(userDao.findById(employee_id));
+        });
+
     }
 }
